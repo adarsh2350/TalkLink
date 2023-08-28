7f284dab98a541452f15337a92fc330a78d52ecf
adarsh2350 <adarsh2350i@gmail.com>
2023-08-28 14:00:44 +0530

    abcd

---
+++ client/src/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import MainContainer from "./Components/MainContainer";
+import Login from "./Components/Login";
+import { Route, Routes } from "react-router-dom";
+import Welcome from "./Components/Welcome";
+import ChatArea from "./Components/ChatArea";
+import Users from "./Components/Users";
+import CreateGroups from "./Components/CreateGroups";
+import Groups from "./Components/Groups";
+import { useSelector } from "react-redux";
+
+function App() {
+  const lightTheme = useSelector((state) => state.themeKey);
+  return (
+    <div className={"App" + (lightTheme ? "" : "-dark")}>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="app" element={<MainContainer />}>
+          <Route path="welcome" element={<Welcome />}></Route>
+          <Route path="chat/:_id" element={<ChatArea />}></Route>
+          <Route path="users" element={<Users />}></Route>
+          <Route path="groups" element={<Groups />}></Route>
+          <Route path="create-groups" element={<CreateGroups />}></Route>
+        </Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
